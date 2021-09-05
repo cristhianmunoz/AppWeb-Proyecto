efc7c8742d481bf88d193d3072dea9edb616d53b
Davidaguirre99 <50632263+Davidaguirre99@users.noreply.github.com>
2021-09-05 03:05:33 -0500

    getters and setters departamento

--- src/main/java/com/Modelo/entidades/Departamento.java
+++ src/main/java/com/Modelo/entidades/Departamento.java
@@ -23,4 +23,17 @@ public class Departamento implements Serializable {
 	private Integer id;
 	@Column(name = "Departamento")
 	private String nombre;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
