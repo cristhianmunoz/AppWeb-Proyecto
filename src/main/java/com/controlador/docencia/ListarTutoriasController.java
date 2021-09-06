5ccbffda06b8d354cdd9b60ac1c55563aa915868
Andrea Cachipuendo <andreavs05@hotmail.com>
2021-09-06 16:16:35 +0200

    Create procesar method in listarTutorias
    Create procesar method join with doGet and doPost method in teh same class ListarTutoriasController

--- src/main/java/com/controlador/docencia/ListarTutoriasController.java
+++ src/main/java/com/controlador/docencia/ListarTutoriasController.java
@@ -1,41 +1,45 @@
 package com.controlador.docencia;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ListarTutoriasController
- */
+import com.Modelo.dao.DAOFactory;
+import com.Modelo.entidades.Estudiante;
+import com.Modelo.entidades.Tutoria;
+
 @WebServlet("/ListarTutoriasController")
 public class ListarTutoriasController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	
+	private static final long serialVersionUID = 1L; 
+	
     public ListarTutoriasController() {
-        super();
-        // TODO Auto-generated constructor stub
+        //super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		procesar(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		procesar(request, response);
 	}
+	
+	protected void procesar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		List<Tutoria> tutorias = DAOFactory.getFactory().getTutoriaDAO().get();
+		
+		//2. Llamo a la vista Listar Tutorias para que me presente los datos que obtuve del modelo
+		request.setAttribute("tutorias", tutorias);
+		String path = "jsp/listarTutorias.jsp";
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+		
+	}
 }
