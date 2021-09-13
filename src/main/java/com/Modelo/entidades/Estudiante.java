757d2deb535bd24ed1911d718009ce99827b0cf2
Davidaguirre99 <50632263+Davidaguirre99@users.noreply.github.com>
2021-09-12 23:28:04 -0500

    constructor Estudiante con todos los atributos
    se generan algunos datos de prueba en generanodoTablas

--- src/main/java/com/Modelo/entidades/Estudiante.java
+++ src/main/java/com/Modelo/entidades/Estudiante.java
@@ -12,6 +12,11 @@ import javax.persistence.*;
 public class Estudiante extends Persona implements Serializable {
 
 	
+	public Estudiante(String cedula, String clave, String nombre, String apellido) {
+		super(cedula, clave, nombre, apellido);
+		// TODO Auto-generated constructor stub
+	}
+
 	private static final long serialVersionUID = 1L;
 
 	public Estudiante() {
--- src/main/java/com/controlador/conexion/generandoTablas.java
+++ src/main/java/com/controlador/conexion/generandoTablas.java
@@ -5,14 +5,28 @@ import com.Modelo.dao.GenericDAO;
 import com.Modelo.entidades.Departamento;
 import com.Modelo.entidades.DisponibilidadTutoria;
 import com.Modelo.entidades.Docente;
+import com.Modelo.entidades.Docente_Disponibilidad;
+import com.Modelo.entidades.Estudiante;
 import com.Modelo.entidades.Horario;
 
 public class generandoTablas {
 
 	public static void main(String[] args) {
-		Departamento d = DAOFactory.getFactory().getDepartamentoDAO().getById(2);
-		Docente doc = new Docente("1111111111","pedro123","Pedro","Gonzales",d);
-		DAOFactory.getFactory().getDocenteDAO().create(doc);		
+		Departamento d =new Departamento("Proyecto Inclusión Digital");
+		DAOFactory.getFactory().getDepartamentoDAO().create(d);
+		Estudiante estudiante = new Estudiante("0000000000","luis123","Luis","Agora");
+		Docente doc = new Docente("222222222","julio123","Julio","Loza",d);
+		DAOFactory.getFactory().getDocenteDAO().create(doc);
+		DAOFactory.getFactory().getEstudianteDAO().create(estudiante);
+		Horario horario = new Horario(8, 30);
+		Horario horarioFin = new Horario(horario.getHora(),horario.getMinuto()+15); 
+		DAOFactory.getFactory().getHorarioDAO().create(horario);
+		DAOFactory.getFactory().getHorarioDAO().create(horarioFin);
+		DisponibilidadTutoria disponibilidad = new DisponibilidadTutoria("Martes", horario, horarioFin);
+		DAOFactory.getFactory().getDisponibilidadTutoriaDAO().create(disponibilidad);
+		Docente_Disponibilidad doc_dispo = new Docente_Disponibilidad(disponibilidad, doc);
+		DAOFactory.getFactory().getDocente_DisponibilidadDAO().create(doc_dispo);
+		
 	}
 
 }
