b6b0d389dbabce88f85e9a38cab199f660dda7a0
Davidaguirre99 <50632263+Davidaguirre99@users.noreply.github.com>
2021-09-18 03:45:35 -0500

    fecha DIa semana dada fecha

--- src/main/java/com/Modelo/dao/FechaDAO.java
+++ src/main/java/com/Modelo/dao/FechaDAO.java
@@ -3,5 +3,8 @@ package com.Modelo.dao;
 import com.Modelo.entidades.Fecha;
 
 public interface FechaDAO extends GenericDAO<Fecha, Integer> {
-
+	public boolean isYearLeap(int anio);
+	public int daysInMonth(int anio, int  month);
+	public int calcularConstSiglo(int siglo);
+	public String determinarDiaDeSemanaDadaUnaFecha(int dia, int mes, int anio);
 }
--- src/main/java/com/Modelo/jpa/JPAFechaDAO.java
+++ src/main/java/com/Modelo/jpa/JPAFechaDAO.java
@@ -10,4 +10,97 @@ public class JPAFechaDAO extends JPAGenericDAO<Fecha, Integer> implements FechaD
 		// TODO Auto-generated constructor stub
 	}
 
+	public boolean isYearLeap(int year) {
+		if (year % 100 == 0 && year > 1581) {
+			if (year % 400 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		if(year%4==0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public int daysInMonth(int year, int month) {
+		if(month==1 || month==3 || month==5 || month==7||month==8||month==10
+				||month==12) {
+			return 31;
+		}
+		if(month==2) {
+			if(isYearLeap(year)) {
+				return 29;
+			}else {
+				return 28;
+			}
+		}else {
+			return 30;
+		}
+	}
+	public int  calcularConstSiglo(int siglo) {
+		if(siglo%2==0 && siglo%4!=0) {
+			return 2;
+		}
+		if(siglo%4==0) {
+			return 0;
+		}
+		int numeroVerificador = 1;
+		int numeroVerificador2=3;
+		while(numeroVerificador<=siglo) {
+			if(numeroVerificador==siglo) {
+				return 3;
+			}
+			numeroVerificador+=4;
+		}
+		while(numeroVerificador2<=siglo) {
+			if(numeroVerificador2==siglo) {
+				return 1;
+			}
+			numeroVerificador2+=4;
+		}
+		return -1;
+	}
+	public String determinarDiaDeSemanaDadaUnaFecha(int dia, int mes, int anio) {
+		String resultado = "";
+		String[] diaCadena = {"Domingo","Lunes","Martes","Miercoles","Jueves", "Viernes","Sábado","Domingo"};
+		int[] monthLeap= {6,2,3,6,1,4,6,2,5,0,3,5};
+		int[] monthNormal= {0,3,3,6,1,4,6,2,5,0,3,5};
+		int[] constSiglo= {0,2,4,6};
+		int anioDigitos=0;
+		int siglo=0;
+		int cocienteAnio=anio;
+		int contador=0;
+		while(cocienteAnio>0) {
+			contador+=1;
+			if(contador==2) {
+				anioDigitos+=(cocienteAnio%10)*10;
+			}
+			if(contador==1) {
+				anioDigitos+=cocienteAnio%10;
+			}
+			if(contador==3) {
+				siglo+=cocienteAnio%10;
+			}
+			if(contador==4) {
+				siglo+=(cocienteAnio%10)*10;
+			}
+			cocienteAnio=Math.floorDiv(cocienteAnio, 10) ;
+		}
+		siglo+=1;
+		int mesConstante;
+		int anioConstante;
+		int numeroDia;
+		if(isYearLeap(anio)) {
+			mesConstante = monthLeap[mes-1];
+			anioConstante = constSiglo[this.calcularConstSiglo(siglo)];
+			numeroDia = (dia+mesConstante+anioDigitos+(Math.floorDiv(anioDigitos, 4))+anioConstante)%7;
+		}else {
+			mesConstante=monthNormal[mes-1];
+			anioConstante=constSiglo[calcularConstSiglo(siglo)];
+			numeroDia=(dia+mesConstante+anioDigitos+(Math.floorDiv(anioDigitos, 4))+anioConstante)%7;
+		}
+		return diaCadena[numeroDia];
+	}
 }
